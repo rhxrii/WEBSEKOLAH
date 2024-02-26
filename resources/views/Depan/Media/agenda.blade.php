184aa551e0623cdaf9de8624a8f1ef444afbca42
imyhacker <arifarhan1602@gmail.com>
2024-02-26 13:17:58 +0700

    Update Meta Image

--- resources/views/Depan/Media/agenda.blade.php
+++ resources/views/Depan/Media/agenda.blade.php
@@ -77,8 +77,8 @@
     <!-- Blog Start -->
     <div class="container">
         <div class="row">
-            <div class="col-md-12">
-            <table class="table table-responsive">
+            <div class="col-md-12 table-responsive">
+            <table class="table" id="tableberita">
                     <thead>
                         <tr>
                             <td>No</td>
