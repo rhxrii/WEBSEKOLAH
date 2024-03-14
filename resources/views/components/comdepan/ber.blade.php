f2841288c0e68acf36263c72cc7e2ea4f5c9c704
imyhacker <arifarhan1602@gmail.com>
2024-03-14 21:41:48 +0700

    Update Berita

--- resources/views/components/comdepan/ber.blade.php
+++ resources/views/components/comdepan/ber.blade.php
@@ -34,14 +34,13 @@
                     </div> -->
                 <div class="card mb-3">
                     <div class="row no-gutters">
-                        <div class="col-md-4 blog-img-inner">
+                        <div class="col-md-6 blog-img-inner">
                             <img src="{{asset('GBERITA/'.$iinf->gberita)}}" class="card-img-top" alt="...">
                         </div>
-                        <div class="col-md-8">
+                        <div class="col-md-6">
                             <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a wider card with supporting text below as a natural
-                                    lead-in to additional content. This content is a little bit longer.</p>
+                                <h5 class="card-title">{{Str::limit($iinf->judul, 10, '....')}}</h5>
+                                <p class="card-text">{!! Str::limit($iinf->deskripsi, 20, '....') !!}</p>
                                 <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                             </div>
                         </div>
