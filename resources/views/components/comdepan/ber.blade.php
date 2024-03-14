f450e70d90c2f66d2a6cbe628da452161f6f31b5
imyhacker <arifarhan1602@gmail.com>
2024-03-14 21:20:54 +0700

    Update Berita

--- resources/views/components/comdepan/ber.blade.php
+++ resources/views/components/comdepan/ber.blade.php
@@ -12,7 +12,7 @@
                     <div class="blog-item">
                         <div class="blog-img">
                             <div class="blog-img-inner">
-                                <img class="img-fluid rounded-top card-berita-top" src="{{asset('GBERITA/'.$iinf->gberita)}}" alt="Image">
+                                <img class="img-fluid rounded-top card-img-top" src="{{asset('GBERITA/'.$iinf->gberita)}}" alt="Image" width="100px" height="100px">
                                 <div class="blog-icon">
                                     <a href="{{route('bacaberita', $iinf->id)}}" class="my-auto"><i class="fas fa-link fa-2x text-white"></i></a>
                                 </div>
