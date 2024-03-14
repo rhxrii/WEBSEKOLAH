5b6859171d5e06f28341af37c8e00f8248ddf08e
imyhacker <arifarhan1602@gmail.com>
2024-03-14 21:36:04 +0700

    Update Berita

--- resources/views/components/comdepan/ber.blade.php
+++ resources/views/components/comdepan/ber.blade.php
@@ -35,7 +35,7 @@
                 <div class="card mb-3" style="max-width: 540px;">
                     <div class="row no-gutters">
                         <div class="col-md-4">
-                            <img src="..." alt="...">
+                            <img src="{{asset('GBERITA/'.$iinf->gberita)}}" class="card-img-top" alt="...">
                         </div>
                         <div class="col-md-8">
                             <div class="card-body">
