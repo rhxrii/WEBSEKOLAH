68c928df178e7feddc0c0f100f031a6ad4eaecc3
imyhacker <arifarhan1602@gmail.com>
2024-03-14 23:03:34 +0700

    Update Galeri Foto

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -63,7 +63,7 @@ class ClientController extends Controller
     }
     public function galerifoto(){
         $link = MLink::get();
-        $foto = Foto::simplePagination(6)->get();
+        $foto = Foto::simplePagination(6);
 
         return view('Depan/Galeri/foto', compact('foto', 'link'));
     }
