2f9c2f40e17b619d490cbb0f9aa78d3229dc6814
imyhacker <arifarhan1602@gmail.com>
2024-03-14 21:34:48 +0700

    Update Berita

--- resources/views/components/comdepan/ber.blade.php
+++ resources/views/components/comdepan/ber.blade.php
@@ -1,15 +1,15 @@
 <div class="container-fluid blog py-5">
-        <div class="container py-5">
-            <div class="mx-auto text-center mb-5" style="max-width: 900px;">
-                <h5 class="section-title px-3">Berita</h5>
-                <h1 class="mb-4">Berita Terupdate</h1>
-                <p class="mb-0">Berita Terupdate dari seputar Sekolah
-                </p>
-            </div>
-            <div class="row justify-content-center">
-                @foreach($ber as $iinf)
-                <div class="col-md-4 mt-3">
-                    <div class="blog-item">
+    <div class="container py-5">
+        <div class="mx-auto text-center mb-5" style="max-width: 900px;">
+            <h5 class="section-title px-3">Berita</h5>
+            <h1 class="mb-4">Berita Terupdate</h1>
+            <p class="mb-0">Berita Terupdate dari seputar Sekolah
+            </p>
+        </div>
+        <div class="row justify-content-center">
+            @foreach($ber as $iinf)
+            <div class="col-md-4 mt-3">
+                <!-- <div class="blog-item">
                         <div class="blog-img">
                             <div class="blog-img-inner" style="width: 18rem;">
                                 <img class="img-fluid rounded-top card-img-top" src="{{asset('GBERITA/'.$iinf->gberita)}}" alt="Image" width="100px" height="100px">
@@ -31,10 +31,25 @@
                             <p class="my-3">{!! Str::limit($iinf->deskripsi, 20, '....') !!}</p>
                             <a href="{{route('bacaberita', $iinf->id)}}" class="btn btn-primary rounded-pill py-2 px-4">Selengkapnya</a>
                         </div>
+                    </div> -->
+                <div class="card mb-3" style="max-width: 540px;">
+                    <div class="row no-gutters">
+                        <div class="col-md-4">
+                            <img src="..." alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural
+                                    lead-in to additional content. This content is a little bit longer.</p>
+                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                @endforeach
-             
             </div>
+            @endforeach
+
         </div>
     </div>
+</div>
